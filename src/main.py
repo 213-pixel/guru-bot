f2aed6011a832a616f96a849b691ef3430fcbafc
213-pixel <kalingga100@gmail.com>
2026-07-30 02:33:55 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
 from aiogram.types import Update
